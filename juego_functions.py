904e5aa580abf1f73199e59d8d54c2e698b4ddce
Nestor Gonzalez <nesgonsua@gmail.com>
2024-02-01 21:19:17 +0000

    Feat: funcion juga

--- juego_functions.py
+++ juego_functions.py
@@ -6,3 +6,12 @@ def crea_tablero(fila, columna):
             lista2.append(0)
         salida.append(lista2)
     return salida
+
+def juega(tablero, columna, valor_ficha):
+    c = tablero[columna]
+    indice = len(c)-1
+    while indice < len(c):
+        if c[indice] == 0:
+            c[indice] = valor_ficha
+            break
+        indice -= 1
--- test_juego.py
+++ test_juego.py
@@ -1,6 +1,13 @@
 from juego_functions import crea_tablero
+from juego_functions import juega
 
 def test_crear_tablero():
     tab = crea_tablero(4, 3)
 
     assert tab == [[0,0,0,0],[0,0,0,0], [0,0,0,0]]
+
+def test_anyadir_ficha_a_tablero():
+    tab = crea_tablero(4,3)
+    juega(tab, 2, 1)
+
+    assert tab == [[0,0,0,0],[0,0,0,0], [0,0,0,1]]
